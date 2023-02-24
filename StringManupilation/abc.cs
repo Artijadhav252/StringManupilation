03d3bd9b13233e8298a727a8cad0a4bfef3cb668
artir <artir@REYANSH2021>
2023-02-24 18:55:31 +0530

    first string program

--- StringManupilation/abc.cs
+++ StringManupilation/abc.cs
@@ -4,22 +4,60 @@ public class Program
 {
     public static void Main()
     {
-        string FirstName, MiddleName, LastName, FullName;
+        //  string FirstName, MiddleName, LastName, FullName;
 
-        Console.WriteLine("Please enter your firstname");
-        FirstName = Console.ReadLine();
-
-
-        Console.WriteLine("Please enter your MiddleNme");
-        MiddleName = Console.ReadLine();
+        // Console.WriteLine("Please enter your firstname");
+        //  FirstName = Console.ReadLine();
 
 
-        Console.WriteLine("Please enter your LastName");
-        LastName = Console.ReadLine();
+        // Console.WriteLine("Please enter your MiddleNme");
+        //  MiddleName = Console.ReadLine();
 
-        FullName = FirstName + " " + MiddleName + " " + LastName;
 
-        Console.WriteLine(FullName);
+        // Console.WriteLine("Please enter your LastName");
+        //  LastName = Console.ReadLine();
 
+        // FullName = FirstName + " " + MiddleName + " " + LastName;
+
+        // Console.WriteLine(FullName);
+
+
+        //calculate length of the string
+
+      //  Console.WriteLine("Please enter any string");
+        //  var str = Console.ReadLine();  //Welcome
+                                         //
+                                         // int counter = 0;
+
+        // foreach (var item in str)
+        // {
+        //    counter++;
+        // }
+        // Console.WriteLine("length of the string ="+counter);
+
+
+        //calculate the vowels of the string
+
+        Console.WriteLine("please enter any  string");
+        var str = Console.ReadLine();  //welcome
+
+        int VowelsCountr = 0;
+        foreach (var item in str)
+           {
+
+            if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u'||
+                item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
+                {
+                VowelsCountr++;
+            }
+           }
+
+        Console.WriteLine("length of the string="+ VowelsCountr);
+        
+
+        
+        
+
+        
     }
 }
