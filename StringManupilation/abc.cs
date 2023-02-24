1f4ddd4c539dd048d92a01f5532c7bb4a07f70bb
artir <artir@REYANSH2021>
2023-02-24 17:49:47 +0530

    First String Program

--- StringManupilation/abc.cs
+++ StringManupilation/abc.cs
@@ -4,6 +4,22 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("please enter any string");
+        string FirstName, MiddleName, LastName, FullName;
+
+        Console.WriteLine("Please enter your firstname");
+        FirstName = Console.ReadLine();
+
+
+        Console.WriteLine("Please enter your MiddleNme");
+        MiddleName = Console.ReadLine();
+
+
+        Console.WriteLine("Please enter your LastName");
+        LastName = Console.ReadLine();
+
+        FullName = FirstName + " " + MiddleName + " " + LastName;
+
+        Console.WriteLine(FullName);
+
     }
 }
